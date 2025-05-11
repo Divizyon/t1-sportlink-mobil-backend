f63afbfb58f390cb84dceaca458be56339c0cd74
Çağrı Tekin <tekincagri16@gmail.com>
2025-05-11 11:48:36 +0300

    remove transports from logger

--- src/config/logger.ts
+++ src/config/logger.ts
@@ -10,8 +10,6 @@ const logger = createLogger({
   ),
   defaultMeta: { service: 'user-service' },
   transports: [
-    new transports.File({ filename: 'logs/error.log', level: 'error' }),
-    new transports.File({ filename: 'logs/combined.log' })
   ]
 });
 
